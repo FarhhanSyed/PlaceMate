8667974487f6bc8fdb5ccb7f9f0e8ac33a249536
Syed Farhan <farhansyed2045@gmail.com>
2025-06-18 23:57:03 +0530

    backend functionalities for authentication

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { use } = require("react");
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, "jwtSecret", { expiresIn: "7d" });
@@ -17,7 +16,7 @@ exports.signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const existing = await User.findOne({ email });
-    if (!existing) {
+    if (existing) {
       return res.status(400).json({ message: "Email already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -27,12 +26,10 @@ exports.signup = async (req, res) => {
       password: hashedPassword,
     });
     generateToken(res, user._id);
-    res
-      .status(201)
-      .json({
-        message: "User registered Successfully",
-        user: { name: user.name, email: user.email },
-      });
+    res.status(201).json({
+      message: "User registered Successfully",
+      user: { name: user.name, email: user.email },
+    });
   } catch (err) {
     res.status(500).json({ message: "SignUp failed" });
   }
@@ -40,7 +37,7 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.find({ email });
+  const user = await User.findOne({ email });
   if (!user) {
     return res.status(400).json({ message: "Invalid Credentials" });
   }
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser=require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db.js");
 const app = express();
-const authRoutes=require("./routes/auth.js");
+const authRoutes = require("./routes/auth.js");
 const User = require("./models/user.js");
 const Quiz = require("./models/quiz.js");
 const Result = require("./models/result.js");
@@ -21,7 +21,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "Cors is working for placemate" });
